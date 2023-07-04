913fe2a0da53c7984c5173cd03b695a8e6adf683
JJVV <junjievv@qq.com>
2023-07-04 12:37:32 +0000

    .

--- toolkit/utils/__init__.py
+++ toolkit/utils/__init__.py
@@ -1,4 +1,2 @@
-from .data import get_dataloader, gradient_accumulate
-from .earlystopping import EarlyStopping
 from .metricdict import MetricDict
-from .training import allocate_gpu_memory, setup_parallel, setup_seed, set_weight_decay
+from .watchdog import WatchDog
