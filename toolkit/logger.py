be5949e2762c8edb65b7158dfd302c15525ab6c2
JJVV <junjievv@qq.com>
2023-07-08 12:26:50 +0000

    .

--- toolkit/logger.py
+++ toolkit/logger.py
@@ -26,23 +26,12 @@ def _getLogger(name: str) -> logging.Logger:
 
 
 def getLogger(name: str, file_path: Path | str) -> logging.Logger:
-    logger = logging.getLogger(name)
-    logger.setLevel(level=logging.DEBUG)
-
-    # 定义日志输出格式
-    log_format = "%(asctime)s - %(log_color)s %(levelname)s > %(name)s: %(message)s"
-
-    # 定义日志输出颜色
-    log_colors = {"DEBUG": "cyan", "INFO": "green", "WARNING": "yellow", "ERROR": "red", "CRITICAL": "red,bg_white"}
-    formatter = colorlog.ColoredFormatter(log_format, log_colors=log_colors)
-    # formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setLevel(logging.DEBUG)
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
+    logger = _getLogger(name)
 
     # 添加handler
+    log_format = "%(asctime)s - %(log_color)s %(levelname)s > %(name)s: %(message)s"
+    log_colors = {"DEBUG": "cyan", "INFO": "green", "WARNING": "yellow", "ERROR": "red", "CRITICAL": "red,bg_white"}
+    formatter = colorlog.ColoredFormatter(log_format, log_colors=log_colors)
     file_handler = logging.FileHandler(file_path)
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(formatter)
