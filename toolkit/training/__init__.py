8b1da1bf256f8a54e3dbfb2b5d412242a715d530
JJVV <junjievv@qq.com>
2023-07-24 21:27:52 +0800

    .

--- toolkit/training/__init__.py
+++ toolkit/training/__init__.py
@@ -1,6 +1,6 @@
 from .checkpoint_manager import CheckpointManager
+from .components import Optimizer, Scaler, Scheduler
+from .dataloader import get_dataloader, gradient_accumulate
 from .initializer import setup_parallel, setup_seed
 from .trainer import Trainer
 from .watchdog import WatchDog
-
-from .components import Optimizer, Scaler, Scheduler
