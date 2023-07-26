a23bcdb13c9e0bb212f6848860b6bff678be4b29
JIE <1454046732@qq.com>
2023-07-27 01:22:15 +0800

    .

--- toolkit/logger.py
+++ toolkit/logger.py
@@ -4,16 +4,25 @@ from pathlib import Path
 
 import colorlog
 
+# class CustomLogRecord(logging.LogRecord):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.levelname = self.levelname.center(7)
+
+
+# logging.setLogRecordFactory(CustomLogRecord)
+
 
 def _getLogger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(level=logging.DEBUG)
 
     # 定义日志输出格式
-    log_format = "%(asctime)s - %(log_color)s %(levelname)s > %(name)s: %(message)s"
+    log_format = "%(asctime)s%(log_color)s <%(levelname)s> %(name)s: %(message)s"
 
     # 定义日志输出颜色
-    log_colors = {"DEBUG": "cyan", "INFO": "green", "WARNING": "yellow", "ERROR": "red", "CRITICAL": "red,bg_white"}
+    # "DEBUG": "white",
+    log_colors = {"INFO": "green", "WARNING": "yellow", "ERROR": "red", "CRITICAL": "red,bg_white"}
     formatter = colorlog.ColoredFormatter(log_format, log_colors=log_colors)
     # formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
@@ -31,8 +40,9 @@ def getLogger(name: str, file_path: Path | str) -> logging.Logger:
     logger = _getLogger(name)
 
     # 添加handler
-    log_format = "%(asctime)s - %(log_color)s %(levelname)s > %(name)s: %(message)s"
-    log_colors = {"DEBUG": "cyan", "INFO": "green", "WARNING": "yellow", "ERROR": "red", "CRITICAL": "red,bg_white"}
+    log_format = "%(asctime)s%(log_color)s <%(levelname)s> %(name)s: %(message)s"
+    # "DEBUG": "white",
+    log_colors = {"INFO": "green", "WARNING": "yellow", "ERROR": "red", "CRITICAL": "red,bg_white"}
     formatter = colorlog.ColoredFormatter(log_format, log_colors=log_colors)
     file_handler = logging.FileHandler(file_path)
     file_handler.setLevel(logging.INFO)
