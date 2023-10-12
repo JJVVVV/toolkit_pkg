1e8651394f09131dbfe1de60130729096e5616c8
JIE <1454046732@qq.com>
2023-10-12 21:13:25 +0800

    .

---
+++ toolkit/metric/rouge.py
@@ -0,0 +1,14 @@
+from torchmetrics.functional.text.rouge import rouge_score
+from .metricdict import MetricDict
+
+
+def calculate_rouge(pred: list[str], tgt: list[str], rouge_keys: str | tuple[str, ...] = "rougeL", language="zh"):
+    """
+    rouge_keys that are allowed are `rougeL`, `rougeLsum`, and `rouge1` through `rouge9`.
+    """
+    ret = MetricDict({key: 0 for key in rouge_keys})
+    if language == "zh":
+        for p, t in zip(pred, tgt):
+            ret_dict = rouge_score(pred, tgt, rouge_keys=rouge_keys, tokenizer=list, normalizer=lambda x: x)
+            ret += MetricDict({key: ret_dict[key + "_fmeasure"] for key in rouge_keys})
+        return ret / len(pred)
