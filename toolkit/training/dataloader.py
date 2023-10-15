4111bed1b662d49772108426584453d88ea1f973
JIE <1454046732@qq.com>
2023-10-15 21:55:44 +0800

    .

--- toolkit/training/dataloader.py
+++ toolkit/training/dataloader.py
@@ -89,7 +89,7 @@ def get_dataloader(
                 (
                     # "The last batch in training data will be discarded! "
                     "The last batch in training is Not strictly batch gradient descent! "
-                    "Because gradient accumulation is enabled, and the last few split batches are less than the accumulate step: "
+                    "Because gradient accumulation is enabled, and the last few micro batches are less than the accumulate step: "
                     f"{tail_batch_num} < {configs.gradient_accumulation_steps}"
                 )
             )
