060b0cc48414023a70e4637f91407e98b10c0ecd
JJVV <1454046732@qq.com>
2023-10-16 17:59:57 +0800

    .

---
+++ toolkit/metric/utils.py
@@ -0,0 +1,6 @@
+def lines_to_ngrams(lines, n=3):
+    ngrams = []
+    for s in lines:
+        words = [e for e in s.replace('.','').replace('\n','').split(' ') if e != '']
+        ngrams.append([tuple(words[i:i + n]) for i in range(len(words) - n + 1)])
+    return ngrams
