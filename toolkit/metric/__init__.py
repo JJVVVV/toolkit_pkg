887849f4474d30901a68c1ec2c02d95849fd9fa1
JJVV <junjievv@qq.com>
2023-10-16 20:47:52 +0800

    .

--- toolkit/metric/__init__.py
+++ toolkit/metric/__init__.py
@@ -1,2 +1,3 @@
 from ._metricdict import MetricDict
 from .similarity_metrics import rouge
+from .diversity_metrics import distinct_n_corpus_level, distinct_n_sentence_level
--- toolkit/metric/diversity_metrics.py
+++ toolkit/metric/diversity_metrics.py
@@ -1,1 +1,25 @@
 from ._metricdict import MetricDict
+from .utils.utils_distinct_n import ngrams
+
+
+def distinct_n_sentence_level(sentence, n):
+    """
+    Compute distinct-N for a single sentence.
+    :param sentence: a list of words.
+    :param n: int, ngram.
+    :return: float, the metric value.
+    """
+    if len(sentence) == 0:
+        return 0.0  # Prevent a zero division
+    distinct_ngrams = set(ngrams(sentence, n))
+    return len(distinct_ngrams) / len(sentence)
+
+
+def distinct_n_corpus_level(sentences, n):
+    """
+    Compute average distinct-N of a list of sentences (the corpus).
+    :param sentences: a list of sentence.
+    :param n: int, ngram.
+    :return: float, the average value.
+    """
+    return sum(distinct_n_sentence_level(sentence, n) for sentence in sentences) / len(sentences)
