55faea56069e46ebe32d3593a66dddb4d1cf18a3
JJVV <junjievv@qq.com>
2023-10-16 20:27:58 +0800

    .

--- toolkit/metric/utils.py
+++ toolkit/metric/utils.py
@@ -4,3 +4,4 @@ def lines_to_ngrams(lines, n=3):
         words = [e for e in s.replace('.','').replace('\n','').split(' ') if e != '']
         ngrams.append([tuple(words[i:i + n]) for i in range(len(words) - n + 1)])
     return ngrams
+
