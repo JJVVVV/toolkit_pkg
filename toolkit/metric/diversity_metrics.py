7ad73255aa958b3e2045c2bbce3e7a02cc592d89
JJVV <1454046732@qq.com>
2023-10-17 11:07:59 +0800

    .

--- toolkit/metric/diversity_metrics.py
+++ toolkit/metric/diversity_metrics.py
@@ -28,12 +28,27 @@ def distinct_n_corpus_level(sentences, n):
     return sum(distinct_n_sentence_level(sentence, n) for sentence in sentences) / len(sentences)
 
 
-def self_bleu_one_set(s: list[str], weights=(0.25, 0.25, 0.25, 0.25), smoothing_level: int = 0, language: str = "zh") -> NDArray:
+bleu_keys2weights = dict(
+    bleu1=(1.0,), bleu2=(1.0 / 2.0, 1.0 / 2.0), bleu3=(1.0 / 3.0, 1.0 / 3.0, 1.0 / 3.0), bleu4=(1.0 / 4.0, 1.0 / 4.0, 1.0 / 4.0, 1.0 / 4.0)
+)
+
+
+def self_bleu_one_set(
+    s: list[str], bleu_keys: str | tuple[str] = "bleu4", weights: tuple[int] | list[tuple[int]] = None, smoothing_level: int = 0, language: str = "zh"
+) -> NDArray:
     """
+    if `weights` is set, the `bleu_keys` will be ignored.
     Smoothing level 0: No smoothing.\n
     Smoothing level 1: Add epsilon counts to precision with 0 counts.\n
     Detail: `https://www.nltk.org/api/nltk.translate.bleu_score.html`
     """
+    if isinstance(bleu_keys, str):
+        bleu_keys = (bleu_keys,)
+    if weights is None:
+        weights = []
+        for key in bleu_keys:
+            weights.append(bleu_keys2weights[key])
+
     chencherry = SmoothingFunction()
     smoothing_function = getattr(chencherry, f"method{smoothing_level}")
 
@@ -49,16 +64,19 @@ def self_bleu_one_set(s: list[str], weights=(0.25, 0.25, 0.25, 0.25), smoothing_
             hyps.append(s[i])
             refs.append([s[j]])
     scores = corpus_bleu(refs, hyps, weights, smoothing_function)
-    # if isinstance(scores, list):
-    #     scores = [1 - score for score in scores]
-    # else:
-    #     scores = 1 - scores
+
     return 1 - np.array(scores)
 
 
-def self_bleu(sets_list: list[list[str]], weights=(0.25, 0.25, 0.25, 0.25), smoothing_level: int = 0, language: str = "zh") -> NDArray:
+def self_bleu(
+    sets_list: list[list[str]],
+    bleu_keys: str | tuple[str] = "bleu4",
+    weights: tuple[int] | list[tuple[int]] = None,
+    smoothing_level: int = 0,
+    language: str = "zh",
+) -> NDArray:
     # sets_list: 一个列表, 其中的每个元素是一个集合, 要计算每个集合中各个元素之间的多样性程度, 然后求平均
     score = 0
     for s in sets_list:
-        score += self_bleu_one_set(s, weights, smoothing_level, language)
+        score += self_bleu_one_set(s, bleu_keys, weights, smoothing_level, language)
     return score / len(sets_list)
