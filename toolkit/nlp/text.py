111225e6db65835d5219756049b5809f7d11111e
JJVV <junjievv@qq.com>
2023-11-16 14:53:50 +0800

    .

--- toolkit/nlp/text.py
+++ toolkit/nlp/text.py
@@ -9,3 +9,15 @@ def word_count(text: str, return_word_list=False) -> int | tuple[int, list[str]]
         return len(word_list)
     else:
         return len(word_list), word_list
+
+def punctuation_convert(text, direction='ch-en'):
+    if direction=='ch-en':
+        table = {ord(f):ord(t) for f,t in zip(
+            u'！？。，；：“”‘’【】「」〈〉〜（）％＃＠＆１２３４５６７８９０',
+            u'!?.,;:\"\"\'\'[]{}<>~()%#@&1234567890')}
+    else:
+        table = {ord(f):ord(t) for f,t in zip(
+            u'!?.,;:\"\"\'\'[]<>~()',
+            u'！？。，；：“”‘’【】〈〉～（）'
+            )}
+    return text.translate(table)
