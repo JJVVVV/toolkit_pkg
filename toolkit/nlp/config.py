d4c342ebac7cfc10f84b7b96bd7503f93f52d7f5
JIE <1454046732@qq.com>
2024-03-23 00:05:04 +0800

    .

--- toolkit/nlp/config.py
+++ toolkit/nlp/config.py
@@ -66,7 +66,7 @@ class NLPTrainingConfig(TrainConfig):
         max_length: int | None = None,
         max_length_input: int | None = None,
         max_length_label: int | None = None,
-        padding_to_max_length:bool=False,
+        padding_to_max_length: bool = False,
         max_new_tokens: int | None = None,
         do_sample: bool = False,
         num_beams: int = 1,
@@ -80,6 +80,8 @@ class NLPTrainingConfig(TrainConfig):
         length_penalty: float = 1.0,
         generation_config_file: Path | str | None = None,
         padding_side: str = "right",
+        model_structure: str | None = None,
+        task_type: str | None = None,
         **kwargs,
     ):
         super().__init__(
@@ -144,6 +146,8 @@ class NLPTrainingConfig(TrainConfig):
         self.max_length_input = max_length_input
         self.max_length_label = max_length_label
         self.padding_to_max_length = padding_to_max_length
+        self.model_structure = model_structure
+        self.task_type = task_type
 
         # generate config
         self.do_sample = do_sample
