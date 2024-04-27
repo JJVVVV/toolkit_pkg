6c64974d96ab6d1bfd1f8be3021911e776cb618e
JIE <1454046732@qq.com>
2024-04-27 14:16:52 +0800

    .

--- toolkit/training/initializer.py
+++ toolkit/training/initializer.py
@@ -83,6 +83,7 @@ def allocate_gpu_memory(ratio=0.8) -> None:
     local_cuda_device_id = cuda_device_ids[local_rank]
     total, used = check_mem(local_cuda_device_id)
     block_mem = int((int(total) - int(used)) * ratio)
+    toolkit_logger.debug("Try to allocate GPU memory ...")
     try:
         x = torch.cuda.FloatTensor(256, 1024, block_mem)
         del x
