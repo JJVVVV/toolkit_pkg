b1b244b5f74e1ac7497c8c8af2f9faf0f14bb002
JJVV <junjievv@qq.com>
2024-06-15 18:05:58 +0800

    .

--- toolkit/chart/histogram.py
+++ toolkit/chart/histogram.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Literal, List
 
 import seaborn as sns
 
@@ -9,7 +9,15 @@ class Histogram(ChartBase):
     def __init__(self, nrows: int = 1, ncols: int = 1, figsize: Tuple = (8, 4.5), dpi: int = 100, is_ch=False) -> None:
         super().__init__(nrows, ncols, figsize, dpi, is_ch)
 
-    def draw(self, data, bins, title="", xlabel="", ylabel="Count", **kwargs) -> None:
+    def draw(
+        self,
+        data,
+        bins: int | Literal["auto", "fd", "doane", "scott", "stone", "rice", "sturges", "sqrt"] | List = 10,
+        title="",
+        xlabel="Value",
+        ylabel="Count",
+        **kwargs,
+    ) -> None:
         colors = sns.color_palette("Greens")
         self.axes[0][0].hist(data, bins=bins, edgecolor="black", color=colors[1], **kwargs)
         for _, ax in enumerate(self.axes.flat):
