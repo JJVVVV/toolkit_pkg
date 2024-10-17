e0e688a9c6d3c387a9bffb238ddeaceac7670560
JJVV <junjievv@qq.com>
2024-10-17 22:32:30 +0800

    .

--- toolkit/metric/__init__.py
+++ toolkit/metric/__init__.py
@@ -1,3 +1,3 @@
-from ._metricdict import MetricDict
+from .metricdict import MetricDict
 from .similarity_metrics import rouge, bleu
 from .diversity_metrics import distinct_n_corpus_level, distinct_n_sentence_level, self_bleu, self_bleu_one_set
