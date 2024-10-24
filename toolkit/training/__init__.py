2e1d57c3b99d7153069c05a7cfc6aad26d7ff526
JJVV <junjievv@qq.com>
2024-10-24 22:11:23 +0800

    通过lazy import优化性能

--- toolkit/training/__init__.py
+++ toolkit/training/__init__.py
@@ -1,9 +1,55 @@
-from .checkpoint_manager import CheckpointManager
-from .components import Optimizer, Scaler, Scheduler
-from .dataloader import get_dataloader, gradient_accumulate
-from .evaluator import Evaluator
-from .initializer import initialize, setup_parallel_ddp, setup_seed
-from .trainer import Trainer
-from .watchdog import WatchDog
-
+# from .components import Optimizer, Scaler, Scheduler
+# from .dataloader import get_dataloader, gradient_accumulate
 # from .loss_functions import cos_loss, kl_loss, mse_loss
+
+
+def lazy_import_Trainer():
+    from .trainer import Trainer
+
+    globals()["Trainer"] = Trainer
+
+
+def lazy_import_initialize():
+    from .initializer import initialize
+
+    globals()["initialize"] = initialize
+
+
+def lazy_import_Evaluator():
+    from .evaluator import Evaluator
+
+    globals()["Evaluator"] = Evaluator
+
+
+def lazy_import_WatchDog():
+    from .watchdog import WatchDog
+
+    globals()["WatchDog"] = WatchDog
+
+
+def lazy_import_ckptManager():
+    from .checkpoint_manager import CheckpointManager
+
+    globals()["CheckpointManager"] = CheckpointManager
+
+
+# __getattr__ 是Python 3.7及以上的特性，用于延迟加载模块
+def __getattr__(name):
+    match name:
+        case "Trainer":
+            lazy_import_Trainer()
+            return globals()[name]
+        case "initialize":
+            lazy_import_initialize()
+            return globals()[name]
+        case "Evaluator":
+            lazy_import_Evaluator()
+            return globals()[name]
+        case "WatchDog":
+            lazy_import_WatchDog()
+            return globals()[name]
+        case "CheckpointManager":
+            lazy_import_ckptManager()
+            return globals()[name]
+        case _:
+            raise AttributeError(f"module {__name__} has no attribute {name}")
